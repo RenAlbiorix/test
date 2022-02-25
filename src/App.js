d6b416e4faef53358fec5ace20f9b204e26bc7f6
Dhaval <dhaval.albiorix@gmail.com>
2022-02-25 09:50:28 +0530

    no message

--- src/App.js
+++ src/App.js
@@ -2,24 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  return (
-    <>
-      <header className="App-header1">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      </>
-  );
+  return(
+    <h1>Hello</h1>
+  )
 }
 
 export default App;
